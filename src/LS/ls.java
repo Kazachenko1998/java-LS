f991cbbe9f50a045c3dc17dee6ed85885e68672d
Kazachenko1998 <kazachenko.1998k@gmail.com>
2017-03-16 21:00:25 +0300

    initial commit

---
+++ src/LS/ls.java
@@ -0,0 +1,8 @@
+package LS;
+
+
+public class ls {
+    public static void main(String[] D) {
+        System.out.print("тест");
+    }
+}
---
+++ test/Tests.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+
+public class Tests {
+    @Test
+    @Tag("deletePoint")
+    void deletePoint() {
+        assertEquals(1, 1);
+    }
+}

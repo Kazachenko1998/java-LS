42e222e9c4cc2178298dc38172b30e1b22bf8ced
Kazachenko1998 <kazachenko.1998k@gmail.com>
2017-03-23 01:51:56 +0300

    написаны тесты

--- src/LS/ls.java
+++ src/LS/ls.java
@@ -52,37 +52,37 @@ public class ls {
     }
 
     private static String[] split_flag(String input) {
-        String[] result = input.split("\\[-");
+        String[] result = input.split(" ");
+        return split_flag(result);
+    }
+    private static String[] split_flag(String[] result) {
         String[] result2 = new String[]{"0", "0", "0", null, null};
         for (int i = 1; i < result.length; i++)
             if (result[i].length() != 0) {
-                char str = result[i].charAt(0);
-                switch (str) {
-                    case 'l':
+                switch (result[i]) {
+                    case "-l":
                         result2[0] = "1";
                         break;
-                    case 'h':
+                    case "-h":
                         result2[1] = "1";
                         break;
-                    case 'r':
+                    case "-r":
                         result2[2] = "1";
                         break;
-                    case 'o': {
-                        String[] file = input.split("\\[-o");
-                        if (file.length > 1)
-                            result2[3] = file[1].split("]")[0];
+                    case "-o": {
+                        if (result.length >= i)
+                            result2[3] = result[i+1];
                         break;
                     }
                     default:
                         break;
                 }
             }
-        String[] file = input.split(" ");
-        result2[4] = file[file.length - 1];
+        result2[4] = result[result.length-1];
         return result2;
     }
 
-    private static String commandLine(String line) {
+    public static String commandLine(String line) {
         if (line.length() < 2) throw new IllegalArgumentException("неверная команда");
         if (!(((line.charAt(0) == 'l') || (line.charAt(0) == 'L')) && ((line.charAt(1) == 's') || (line.charAt(1) == 'S'))))
             throw new IllegalArgumentException("неверная команда");
@@ -150,10 +150,89 @@ public class ls {
         return "";
     }
 
+
+    private static String commandLine(String[] line) {
+        if (line.length == 0) throw new IllegalArgumentException("неверная команда");
+        if (!(line[0].equals("ls")))
+            throw new IllegalArgumentException("неверная команда");
+        String result = new String();
+        File file = new File(split_flag(line)[4]);
+        String[] str = file.list();
+        if (file.isFile()) str = new String[]{split_flag(line)[4]};
+        String[] flags = split_flag(line);
+        ArrayList<String> list = new ArrayList<>();
+        int size = 0;
+        try {
+            for (int i = 0; i < str.length; i++) {
+                if (str[i].length() > size) size = str[i].length();
+            }
+        } catch (NullPointerException ex) {
+            throw new NullPointerException("неверный входной путь");
+        }
+        if (file.isDirectory()) for (int i = 0; i < str.length; i++) {
+            String space = new String();
+            for (int j = str[i].length(); j < size + 1; j++)
+                space += " ";
+            if (flags[0] == "0") list.add(str[i]);
+            if ((flags[0] == "1") && (flags[1] == "0")) list.add(
+                    file.getPath() + "\\" + str[i] + space + " " +
+                            accessBite(new File(file.getPath() + "\\" + str[i])) + " "
+                            + (new File(file.getPath() + "\\" + str[i])).lastModified() + " "
+                            + sizeFile(new File(file.getPath() + "\\" + str[i])));
+            if ((flags[0] == "1") && (flags[1] == "1")) list.add(
+                    str[i] + space + " " +
+                            accessHuman(new File(file.getPath() + "\\" + str[i])) + " "
+                            + data(new File(file.getPath() + "\\" + str[i])) + " "
+                            + sizeFileHuman(new File(file.getPath() + "\\" + str[i])));
+        }
+        if (file.isFile()) for (int i = 0; i < str.length; i++) {
+            String space = new String();
+            for (int j = str[i].length(); j < size + 1; j++)
+                space += " ";
+            if (flags[0] == "0") list.add(str[i]);
+            if ((flags[0] == "1") && (flags[1] == "0")) list.add(
+                    file.getPath() + space + " " +
+                            accessBite(file) + " "
+                            + file.lastModified() + " "
+                            + sizeFile(file));
+            if ((flags[0] == "1") && (flags[1] == "1")) list.add(
+                    str[i] + space + " " +
+                            accessHuman(file) + " "
+                            + data(file) + " "
+                            + sizeFileHuman(file));
+        }
+        if (flags[2] == "1") for (int i = list.size() - 1; i >= 0; i--)
+            result += list.get(i) + "\n";
+        else for (int i = 0; i < list.size(); i++)
+            result += list.get(i) + "\n";
+        if (flags[3] == null)
+            return result;
+        else {
+            try (
+                    FileWriter writer = new FileWriter(flags[3], false)) {
+                writer.write(result);
+                writer.flush();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return "";
+    }
+
     public static void main(String[] D) throws Exception {
-        //     BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
-        //       String input = reader.readLine();
-        String input = "ls [-l][-h]  C:\\Windows";
-        System.out.print(commandLine(input));
+//     BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+//       String input = reader.readLine();
+//        String[] input2 = D;
+//        for (int i = 0;i<D.length;i++)
+//            System.out.println(D[i]);
+        String input = "ls -l -h -o help C:\\";
+        System.out.print(commandLine("ls -o __f testIn\\Ace_Stream_Media_3.1.2.exe"));
+        System.out.print(commandLine("ls -l -o l_f testIn\\Ace_Stream_Media_3.1.2.exe"));
+        System.out.print(commandLine("ls -l -h -o lhf testIn\\Ace_Stream_Media_3.1.2.exe"));
+        System.out.print(commandLine("ls  -o __d testIn"));
+        System.out.print(commandLine("ls -l  -o l_d testIn"));
+        System.out.print(commandLine("ls -l -h -o lhd testIn"));
+        //       System.out.print(commandLine(D));
+
     }
 }
--- test/Tests.java
+++ test/Tests.java
@@ -1,12 +1,171 @@
+import static LS.ls.commandLine;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
 
+import java.io.*;
+
 public class Tests {
+    private void assertFileContent( String name, String expectedContent) throws IOException {
+        String content = new String();
+        try(FileReader reader = new FileReader(name))
+        {
+            int c;
+            while((c=reader.read())!=-1){
+                content+=(char)c;
+            }
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        assertEquals(expectedContent, content);
+    }
     @Test
-    @Tag("deletePoint")
-    void deletePoint() {
-        assertEquals(1, 1);
+    @Tag("---d")
+    void __od() throws IOException {
+        assertFileContent("testOut\\__d", "Ace_Stream_Media_3.1.2.exe\n" +
+                "AdbeRdr930_ru_RU.exe\n" +
+                "atheros_ar5xxx_ar9xxx_wireless_10_0_0_298_whql.zip\n" +
+                "avast_free_antivirus_setup_online_comss.exe\n" +
+                "Delphi7_Lite_Full_Setup_v7.3.3.3v3(Build_2010-02-02).exe\n" +
+                "DTLiteInstaller.exe\n" +
+                "Git-2.10.0-64-bit.exe\n" +
+                "K-Lite_Codec_Pack_960_Mega.exe\n" +
+                "KMPlayer 2.9.4.1434 Final.exe\n" +
+                "mseinstall.exe\n" +
+                "online-consultant.exe\n" +
+                "TeamViewerPortable\n" +
+                "teeworlds-0.5.1-win32\n" +
+                "Tor Browser\n" +
+                "тема\n");
+    }
+    @Test
+    @Tag("l--f")
+    void __of() throws IOException {
+        assertFileContent("testOut\\__f", "testIn\\Ace_Stream_Media_3.1.2.exe\n");
+    }
+    @Test
+    @Tag("lh-f")
+    void lhof() throws IOException {
+        assertFileContent("testOut\\lhf", "testIn\\Ace_Stream_Media_3.1.2.exe  xrw   последние изменение 18.03.2016 05:33:05   75MB\n");
+
+    }
+    @Test
+    @Tag("---d")
+    void l_of() throws IOException {
+        assertFileContent("testOut\\l_f", "testIn\\Ace_Stream_Media_3.1.2.exe  111 1458311585083 79522432\n");
+    }
+    @Test
+    @Tag("l--d")
+    void l_od() throws IOException {
+        assertFileContent("testOut\\l_d", "testIn\\Ace_Stream_Media_3.1.2.exe                                111 1458311585083 79522432\n" +
+                "testIn\\AdbeRdr930_ru_RU.exe                                      111 1279732176000 25879608\n" +
+                "testIn\\atheros_ar5xxx_ar9xxx_wireless_10_0_0_298_whql.zip        111 1438626227247 81475851\n" +
+                "testIn\\avast_free_antivirus_setup_online_comss.exe               111 1438876146121 5481336\n" +
+                "testIn\\Delphi7_Lite_Full_Setup_v7.3.3.3v3(Build_2010-02-02).exe  111 1273572330000 65762043\n" +
+                "testIn\\DTLiteInstaller.exe                                       111 1438952882607 1709792\n" +
+                "testIn\\Git-2.10.0-64-bit.exe                                     111 1473356327247 33049944\n" +
+                "testIn\\K-Lite_Codec_Pack_960_Mega.exe                            111 1355557062417 24222983\n" +
+                "testIn\\KMPlayer 2.9.4.1434 Final.exe                             111 1338740319172 29391072\n" +
+                "testIn\\mseinstall.exe                                            111 1386123358172 13697208\n" +
+                "testIn\\online-consultant.exe                                     111 1450679908864 15745976\n" +
+                "testIn\\TeamViewerPortable                                        111 1490211686138 4096\n" +
+                "testIn\\teeworlds-0.5.1-win32                                     111 1490211585663 0\n" +
+                "testIn\\Tor Browser                                               111 1490211628510 0\n" +
+                "testIn\\тема                                                      111 1490211649326 4096\n");
+    }
+    @Test
+    @Tag("lh-d")
+    void lhod() throws IOException {
+        assertFileContent("testOut\\lhd", "Ace_Stream_Media_3.1.2.exe                                xrw   последние изменение 18.03.2016 05:33:05   75MB\n" +
+                "AdbeRdr930_ru_RU.exe                                      xrw   последние изменение 21.07.2010 09:09:36   24MB\n" +
+                "atheros_ar5xxx_ar9xxx_wireless_10_0_0_298_whql.zip        xrw   последние изменение 03.08.2015 09:23:47   77MB\n" +
+                "avast_free_antivirus_setup_online_comss.exe               xrw   последние изменение 06.08.2015 06:49:06   5MB\n" +
+                "Delphi7_Lite_Full_Setup_v7.3.3.3v3(Build_2010-02-02).exe  xrw   последние изменение 11.05.2010 02:05:30   62MB\n" +
+                "DTLiteInstaller.exe                                       xrw   последние изменение 07.08.2015 04:08:02   1MB\n" +
+                "Git-2.10.0-64-bit.exe                                     xrw   последние изменение 08.09.2016 08:38:47   31MB\n" +
+                "K-Lite_Codec_Pack_960_Mega.exe                            xrw   последние изменение 15.12.2012 11:37:42   23MB\n" +
+                "KMPlayer 2.9.4.1434 Final.exe                             xrw   последние изменение 03.06.2012 08:18:39   28MB\n" +
+                "mseinstall.exe                                            xrw   последние изменение 04.12.2013 06:15:58   13MB\n" +
+                "online-consultant.exe                                     xrw   последние изменение 21.12.2015 09:38:28   15MB\n" +
+                "TeamViewerPortable                                        xrw   последние изменение 22.03.2017 10:41:26   (Папка) 4KB\n" +
+                "teeworlds-0.5.1-win32                                     xrw   последние изменение 22.03.2017 10:39:45   (Папка) 0Bait\n" +
+                "Tor Browser                                               xrw   последние изменение 22.03.2017 10:40:28   (Папка) 0Bait\n" +
+                "тема                                                      xrw   последние изменение 22.03.2017 10:40:49   (Папка) 4KB\n");
+    }
+    @Test
+    @Tag("---d")
+    void ___d() throws IOException {
+        assertEquals(commandLine("ls testIn"), "Ace_Stream_Media_3.1.2.exe\n" +
+                "AdbeRdr930_ru_RU.exe\n" +
+                "atheros_ar5xxx_ar9xxx_wireless_10_0_0_298_whql.zip\n" +
+                "avast_free_antivirus_setup_online_comss.exe\n" +
+                "Delphi7_Lite_Full_Setup_v7.3.3.3v3(Build_2010-02-02).exe\n" +
+                "DTLiteInstaller.exe\n" +
+                "Git-2.10.0-64-bit.exe\n" +
+                "K-Lite_Codec_Pack_960_Mega.exe\n" +
+                "KMPlayer 2.9.4.1434 Final.exe\n" +
+                "mseinstall.exe\n" +
+                "online-consultant.exe\n" +
+                "TeamViewerPortable\n" +
+                "teeworlds-0.5.1-win32\n" +
+                "Tor Browser\n" +
+                "тема\n");
+    }
+    @Test
+    @Tag("l--f")
+    void ___f() throws IOException {
+        assertEquals(commandLine("ls testIn\\Ace_Stream_Media_3.1.2.exe"), "testIn\\Ace_Stream_Media_3.1.2.exe\n");
+    }
+
+    @Test
+    @Tag("lh-f")
+    void lh_f() throws IOException {
+        assertEquals(commandLine("ls -l -h testIn\\Ace_Stream_Media_3.1.2.exe"), "testIn\\Ace_Stream_Media_3.1.2.exe  xrw   последние изменение 18.03.2016 05:33:05   75MB\n");
+    }
+
+    @Test
+    @Tag("---d")
+    void l__f() throws IOException {
+        assertEquals(commandLine("ls -l testIn\\Ace_Stream_Media_3.1.2.exe"),"testIn\\Ace_Stream_Media_3.1.2.exe  111 1458311585083 79522432\n");
+    }
+    @Test
+    @Tag("l--d")
+    void l__d() throws IOException {
+        assertEquals(commandLine("ls -l  testIn"), "testIn\\Ace_Stream_Media_3.1.2.exe                                111 1458311585083 79522432\n" +
+                "testIn\\AdbeRdr930_ru_RU.exe                                      111 1279732176000 25879608\n" +
+                "testIn\\atheros_ar5xxx_ar9xxx_wireless_10_0_0_298_whql.zip        111 1438626227247 81475851\n" +
+                "testIn\\avast_free_antivirus_setup_online_comss.exe               111 1438876146121 5481336\n" +
+                "testIn\\Delphi7_Lite_Full_Setup_v7.3.3.3v3(Build_2010-02-02).exe  111 1273572330000 65762043\n" +
+                "testIn\\DTLiteInstaller.exe                                       111 1438952882607 1709792\n" +
+                "testIn\\Git-2.10.0-64-bit.exe                                     111 1473356327247 33049944\n" +
+                "testIn\\K-Lite_Codec_Pack_960_Mega.exe                            111 1355557062417 24222983\n" +
+                "testIn\\KMPlayer 2.9.4.1434 Final.exe                             111 1338740319172 29391072\n" +
+                "testIn\\mseinstall.exe                                            111 1386123358172 13697208\n" +
+                "testIn\\online-consultant.exe                                     111 1450679908864 15745976\n" +
+                "testIn\\TeamViewerPortable                                        111 1490211686138 4096\n" +
+                "testIn\\teeworlds-0.5.1-win32                                     111 1490211585663 0\n" +
+                "testIn\\Tor Browser                                               111 1490211628510 0\n" +
+                "testIn\\тема                                                      111 1490211649326 4096\n");
+    }
+    @Test
+    @Tag("lh-d")
+    void lh_d() throws IOException {
+        assertEquals(commandLine("ls -l -h testIn"), "Ace_Stream_Media_3.1.2.exe                                xrw   последние изменение 18.03.2016 05:33:05   75MB\n" +
+                "AdbeRdr930_ru_RU.exe                                      xrw   последние изменение 21.07.2010 09:09:36   24MB\n" +
+                "atheros_ar5xxx_ar9xxx_wireless_10_0_0_298_whql.zip        xrw   последние изменение 03.08.2015 09:23:47   77MB\n" +
+                "avast_free_antivirus_setup_online_comss.exe               xrw   последние изменение 06.08.2015 06:49:06   5MB\n" +
+                "Delphi7_Lite_Full_Setup_v7.3.3.3v3(Build_2010-02-02).exe  xrw   последние изменение 11.05.2010 02:05:30   62MB\n" +
+                "DTLiteInstaller.exe                                       xrw   последние изменение 07.08.2015 04:08:02   1MB\n" +
+                "Git-2.10.0-64-bit.exe                                     xrw   последние изменение 08.09.2016 08:38:47   31MB\n" +
+                "K-Lite_Codec_Pack_960_Mega.exe                            xrw   последние изменение 15.12.2012 11:37:42   23MB\n" +
+                "KMPlayer 2.9.4.1434 Final.exe                             xrw   последние изменение 03.06.2012 08:18:39   28MB\n" +
+                "mseinstall.exe                                            xrw   последние изменение 04.12.2013 06:15:58   13MB\n" +
+                "online-consultant.exe                                     xrw   последние изменение 21.12.2015 09:38:28   15MB\n" +
+                "TeamViewerPortable                                        xrw   последние изменение 22.03.2017 10:41:26   (Папка) 4KB\n" +
+                "teeworlds-0.5.1-win32                                     xrw   последние изменение 22.03.2017 10:39:45   (Папка) 0Bait\n" +
+                "Tor Browser                                               xrw   последние изменение 22.03.2017 10:40:28   (Папка) 0Bait\n" +
+                "тема                                                      xrw   последние изменение 22.03.2017 10:40:49   (Папка) 4KB\n");
     }
 }
